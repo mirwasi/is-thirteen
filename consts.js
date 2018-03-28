89c65b6ad7946d22b7cd7b9ace97d2e34be4c892
Mir Wasi Ahmed <wasi2005@gmail.com>
2018-03-28 16:57:52 +0100

    Added Bengali meanings of thirteen.

--- consts.js
+++ consts.js
@@ -254,7 +254,9 @@ var thirteenStrings = [
     "β", //Think this is beta, which looks like a long 1 mashed together with a 3
     // Thirteen pronunciation
     "θərˈtiːn",
-    "పదమూడు" //Telugu
+    "పదమూడు", //Telugu,
+    "তেরো", // Bengali
+    "তের" // Bengali (alternate spelling)
 ];
 
 module.exports = {
